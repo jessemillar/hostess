d89ada00b207d3a4b850a2a35d504ac442644284
Jesse Millar <hellojessemillar@gmail.com>
2022-08-14 20:58:13 -0500

    Adding a version number for the pipeline

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+const version = "1.0.0"
+
 func main() {
 	port := flag.String("p", "8080", "port to serve on")
 	directory := flag.String("d", ".", "the directory of static files to serve")
